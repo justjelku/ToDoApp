0083e6d5724ba7add17803522d1771e0021f64de
Jeremiah Larry King Ungsod <67980301+Jeremiah019@users.noreply.github.com>
2022-12-24 07:21:02 +0800

    todo model

---
+++ lib/todo.dart
@@ -0,0 +1,28 @@
+class Todo {
+  final int id;
+  final String title;
+  final String description;
+
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      // it doesn't support the boolean either, so we save that as integer.
+    };
+  }
+
+  // this function is for debugging only
+  @override
+  String toString() {
+    return 'Todo(id : $id, title : $title, description : $description)';
+  }
+}
